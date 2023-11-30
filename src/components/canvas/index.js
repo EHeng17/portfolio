0fa27cc026bf07f06059e890b592030254098263
Chua E Heng <eheng@whenso.com>
2023-11-30 15:58:37 +0800

    Change naming and logo

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -1,6 +1,6 @@
 import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
-import HologramCanvas from "./Computers";
+import HologramCanvas from "./Hologram";
 import StarsCanvas from "./Stars";
 
 export { EarthCanvas, BallCanvas, HologramCanvas, StarsCanvas };
